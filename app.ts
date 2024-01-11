8259e84a85fc613b18a57a8563bec1ab8e180f6e
627km <rudals_jeon@naver.com>
2024-01-11 17:16:51 +0900

    feat: change from typeAlias to interface

--- app.ts
+++ app.ts
@@ -1,33 +1,33 @@
 // 타입 알리아스 (type alias)
-type Store = {
+interface Store {
     currentPage: number;
     feeds: NewsFeed[];
-};
+}
 
 // 중복 된 타입
-type News = {
-    id: number;
-    time_ago: string;
-    title: string;
-    url: string;
-    user: string;
-    content: string;
-};
+interface News {
+    readonly id: number;
+    readonly time_ago: string;
+    readonly title: string;
+    readonly url: string;
+    readonly user: string;
+    readonly content: string;
+}
 
-type NewsFeed = News & {
-    comments_count: number;
-    points: number;
+interface NewsFeed extends News {
+    readonly comments_count: number;
+    readonly points: number;
     read?: boolean; // ?를 붙이면 항상있는 것이 아닌 선택적으로 있는 데이터라는 의미이다.
-};
+}
 
-type NewsDetail = News & {
-    comments: NewsComment[];
-};
+interface NewsDetail extends News {
+    readonly comments: NewsComment[];
+}
 
-type NewsComment = News & {
-    comments: NewsComment[];
-    level: number;
-};
+interface NewsComment extends News {
+    readonly comments: NewsComment[];
+    readonly level: number;
+}
 
 const container: HTMLElement | null = document.getElementById("root");
 const ajax: XMLHttpRequest = new XMLHttpRequest();
